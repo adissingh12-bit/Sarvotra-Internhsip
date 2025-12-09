2aa91b3229ba692001eaf462140e2b23c797d06b
adissingh12-bit <adissingh12@gmail.com>
2025-12-10 00:32:22 +0530

    Update App.tsx

--- sarvotra Internship app/App.tsx
+++ sarvotra Internship app/App.tsx
@@ -5,7 +5,7 @@ import VoiceAgent from './components/VoiceAgent';
 import BalanceCard from './components/BalanceCard';
 import ContactList from './components/ContactList';
 import TransactionList from './components/TransactionList';
-import ContactPayment from './components/ContactPayment'; // Updated import
+import PaymentView from './components/PaymentView';
 import ActionModal, { BankDetails } from './components/ActionModal';
 
 const App: React.FC = () => {
@@ -125,7 +125,7 @@ const App: React.FC = () => {
       case 'pay':
         if (selectedContact) {
             return (
-                <ContactPayment 
+                <PaymentView 
                     contact={selectedContact}
                     transactions={userState.transactions.filter(t => t.recipientId === selectedContact.id)}
                     onBack={() => setSelectedContact(null)}
