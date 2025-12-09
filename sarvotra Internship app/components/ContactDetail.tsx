6d1ef5ef1a479604b0160a2d7ba09db71312c550
adissingh12-bit <adissingh12@gmail.com>
2025-12-10 00:17:12 +0530

    Delete sarvotra Internship app/components/ContactDetail.tsx

--- sarvotra Internship app/components/ContactDetail.tsx
+++
@@ -1,3 +0,0 @@
-// This file is kept as a redirect to fix potential build caching issues.
-// You can delete this file once the build stabilizes and App.tsx is confirmed to import ContactPayment.
-export { default } from './ContactPayment';
